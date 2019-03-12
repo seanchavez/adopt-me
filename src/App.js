df961e7ee484e24b690f00ff0bd3a1784e871812
Sean Chavez <realseanchavez@gmail.com>
2019-03-12 14:01:37 -0700

    fix some crazy ass shit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
+  return React.createElement("h1", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
     React.createElement("h2", {}, breed)
-  ]);
-};
+  ])
+}
+
 
 const App = () => {
   return React.createElement("div", {}, [
