221545e636c13d9fa6f320fabcdb062bc3557cb1
Sean Chavez <realseanchavez@gmail.com>
2019-03-13 16:02:40 -0700

    hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "petfinder-client";
 
 const SearchParams = () => {
-  const location = "Temecula, CA";
+  //const location = "Temecula, CA";
+  const [location, setLocation] = useState("Temecula, CA");
+  const [animal, setAnimal] = useState("dog");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor={"location"}>
           Location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            onChange={e => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+          >
+            <option />
+          </select
         </label>
         <button>Submit</button>
       </form>
