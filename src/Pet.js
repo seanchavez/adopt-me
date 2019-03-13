3b54ac294dc475a95773e82c03953cbab0755a66
Sean Chavez <realseanchavez@gmail.com>
2019-03-13 14:46:52 -0700

    debug the things

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,18 @@
 import React from "react";
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement("h1", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+
+  // return React.createElement("h1", {}, [
+  //   React.createElement("h1", {}, name),
+  //   React.createElement("h2", {}, animal),
+  //   React.createElement("h2", {}, breed)
+  // ]);
 };
 
 export default Pet;
---
+++ src/SearchParams.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchParams = () => {
+  const location = "Temecula, CA";
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor={"location"}>
+          Location
+          <input id="location" value={location} placeholder="location" />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
