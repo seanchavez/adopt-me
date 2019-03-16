c5b8a25f1e187bba234b80f311ddf3978ca354f0
Sean Chavez <realseanchavez@gmail.com>
2019-03-16 14:01:13 -0700

    create Navbar

---
+++ src/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "@reach/router";
+import { css } from "@emotion/core";
+
+const NavBar = () => (
+  <header
+    css={css`
+      background-color: #333;
+      position: sticky;
+      top: 0;
+      z-index: 10;
+    `}
+  >
+    <Link to="/">Adopt Me!</Link>
+    <Link to="/">
+      <span aria-label="logo" role="img">
+        🐶
+      </span>
+    </Link>
+  </header>
+);
+
+export default NavBar;
