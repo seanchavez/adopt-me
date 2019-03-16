56b1eb346a5544b44d186db5bb845387c38d6ada
Sean Chavez <realseanchavez@gmail.com>
2019-03-15 17:27:29 -0700

    ErrorBoundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import pf from "petfinder-client";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -50,4 +51,10 @@ class Details extends Component {
   }
 }
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
