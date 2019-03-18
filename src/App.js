9b62414ce4fd693801fe9fc207c0862522c271a0
Sean Chavez <realseanchavez@gmail.com>
2019-03-17 19:13:23 -0700

    spin up express server and refactor application for ssr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
@@ -28,4 +27,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
